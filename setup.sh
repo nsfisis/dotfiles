a3171b6da8a2792264d55d4f01eafc7fd6445a4a
nsfisis <nsfisis@gmail.com>
2021-05-30 20:23:10 +0900

    Move '.gitignore_global' to '.config/git/ignore'
    It is the default place of the global ignore file.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-for filename in .gitconfig .gitignore_global .vim .vimrc .zshrc
+for filename in .gitconfig .vim .vimrc .zshrc
 do
     rm -f ~/$filename
     ln -s ~/dotfiles/$filename ~/$filename
 done
 
 [ -d ~/.config ] || mkdir ~/.config
-for dirname in alacritty
+for dirname in alacritty git
 do
     rm -f ~/.config/$dirname
     ln -s ~/dotfiles/.config/$dirname ~/.config/$dirname
