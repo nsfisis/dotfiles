851b47f00b199a6454a47945eaf065f5a183185c
nsfisis <nsfisis@gmail.com>
2021-06-01 22:23:49 +0900

    Revert "fish shell"
    This reverts commit 77db37d3506edd006c0d4edc8b216d869a06f30b.

--- .config/fish/functions/256colors.fish
+++
@@ -1,5 +0,0 @@
-function 256colors
-    for code in (seq 0 255)
-        echo -e '\e[38;05;'$code'm '(printf '%3d' $code)': Test'
-    end
-end
--- .config/fish/functions/mkcd.fish
+++
@@ -1,9 +0,0 @@
-function mkcd
-    if [ (count $argv) != 1 ]
-        echo "Usage: mkcd DIR"
-        return 1
-    end
-
-    mkdir -p $argv[1]
-    cd $argv[1]
-end
