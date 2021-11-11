74681e966d715bc022aad5b4d652f0737735ea50
nsfisis <nsfisis@gmail.com>
2021-11-11 14:30:45 +0900

    setup.sh: download plug.vim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f ~/dotfiles/.vim/autoload/plug.vim ]; then
+    echo "download: ~/dotfiles/.vim/autoload/plug.vim"
+    curl -fLo ~/dotfiles/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 for name in \
     .emacs.d \
     .gitconfig \
