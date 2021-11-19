544aca897346feaecab01425993a22a2183bc271
nsfisis <nsfisis@gmail.com>
2021-11-19 23:27:34 +0900

    neovim: add -n flag to scripts/vim-startup.sh

--- scripts/vim-startup.sh
+++ scripts/vim-startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$1" == '-n' ]]; then
+    vim=nvim
+else
+    vim=vim
+fi
+
 log="$(mktemp)"
-vim --startuptime "$log" +q
-sort -nr -k2 "$log" | head -n 30 > ./vim-startup.log
+$vim --startuptime "$log" +q
+sort -nr -k2 "$log" | head -n 30 > ./$vim-startup.log
