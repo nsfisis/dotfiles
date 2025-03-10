23e245aef9953eaeefe79a01bbfa8d1753b3dfbf
nsfisis <nsfisis@gmail.com>
2025-03-11 03:23:11 +0900

    nvim: install nvim-ts-autotag

--- .config/nvim/lua/vimrc/plugins.lua
+++ .config/nvim/lua/vimrc/plugins.lua
@@ -369,6 +369,17 @@ return {
          })
       end,
    },
+   -- HTML
+   {
+      'windwp/nvim-ts-autotag',
+      opts = {
+         opts = {
+            enable_close = true,
+            enable_rename = true,
+            enable_close_on_slash = true,
+         },
+      },
+   },
    -- Markdown
    {
       'MeanderingProgrammer/render-markdown.nvim',
