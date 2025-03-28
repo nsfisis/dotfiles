1e553a94ff39990f6615c211d9516d688a577aeb
nsfisis <nsfisis@gmail.com>
2025-03-28 22:17:49 +0900

    home-manager: fix $PATH on macOS

--- home-manager/config/fish/path.fish
+++ home-manager/config/fish/path.fish
@@ -1,3 +1,14 @@
 fish_add_path $HOME/go/bin $HOME/.cargo/bin $HOME/.deno/bin $HOME/.local/bin $HOME/bin
-# https://github.com/LnL7/nix-darwin/issues/122
-fish_add_path --move $HOME/.nix-profile/bin
+
+# For macOS
+if test (uname) = "Darwin"
+    set -l nix_profile_dir /nix/var/nix/profiles/default
+    if test -f $nix_profile_dir/etc/profile.d/nix-daemon.fish
+        source $nix_profile_dir/etc/profile.d/nix-daemon.fish
+    end
+    if test -f $nix_profile_dir/share/fish/vendor_completions.d/nix.fish
+        source $nix_profile_dir/share/fish/vendor_completions.d/nix.fish
+    end
+    # https://github.com/LnL7/nix-darwin/issues/122
+    fish_add_path --move $HOME/.nix-profile/bin
+end
