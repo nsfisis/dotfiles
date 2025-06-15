4d1a502e9bb425e2802cfed4eec421059c396ce8
nsfisis <nsfisis@gmail.com>
2025-06-15 13:14:12 +0900

    nvim: uninstall copilot

--- .config/nvim/lua/vimrc/plugins.lua
+++ .config/nvim/lua/vimrc/plugins.lua
@@ -777,18 +777,6 @@ return {
          })
       end,
    },
-   -- *Magic* {{{1
-   -- Integration with GitHub Copilot (https://docs.github.com/en/copilot)
-   {
-      'github/copilot.vim',
-      init = function()
-         vim.g.copilot_filetypes = {
-            djot = false,
-            markdown = false,
-            qfreplace = false,
-            text = false,
-         }
-      end,
-   },
+   -- AI {{{1
    -- }}}
 }
